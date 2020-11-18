1a7d733fa76bba7311b742013f198a9d87ede4a0
STYJ <simon.tan.yu.jing@gmail.com>
2020-11-18 13:37:23 +0800

    updated panic message for environment mod

--- src/environment.rs
+++ src/environment.rs
@@ -5,18 +5,10 @@ use std::env;
 
 pub fn load() {
     dotenv().ok();
-    set_teloxide_env_var();
 }
 
 pub fn get_value(key: &str) -> String
 {
-    let val = env::var(key).expect(&format!("key \"{}\" not found in .env", key));
+    let val = env::var(key).expect(&format!("The key \"{}\" is not found in your .env", key));
     val
-}
-
-fn set_teloxide_env_var() {
-    let key = "TELEGRAM_KEY";
-    let val = get_value(key);
-    env::set_var("TELOXIDE_TOKEN", &val);
-    assert_eq!(env::var(key), Ok(val.to_string()));
 }
