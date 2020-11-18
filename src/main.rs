2862c138a5c7ae161166bc6ace8f99145c37bddc
STYJ <simon.tan.yu.jing@gmail.com>
2020-11-18 19:18:52 +0800

    synchronising latest changes

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,10 @@
-use std::time;
-use web3::contract::{Contract, Options};
-use web3::futures::StreamExt;
-use web3::types::{Address, FilterBuilder};
+mod uniswap;
+mod environment;
 
 #[tokio::main]
 async fn main() -> web3::contract::Result<()> {
     let _ = env_logger::try_init();
-    let transport = web3::transports::WebSocket::new("wss://mainnet.infura.io/ws/v3/b955965c36374388a7c874e91b511d79").await?;
-    let web3 = web3::Web3::new(transport);
-    log::info!("{:?}", web3);
-
-    // let address: Address = "52bc44d5378309EE2abF1539BF71dE1b7d7bE3b5".parse().unwrap();
-    // let balance = web3.eth().balance(address, Some(BlockNumber::Latest)).await?;
-    // log::info!("{:?}", balance);
-
-    // let me: Address = "267be1C1D684F78cb4F6a176C4911b741E4Ffdc0".parse().unwrap();
-    let uni_contract_address: Address = "1f9840a85d5aF5bf1D1762F925BDADdC4201F984".parse().unwrap();
-    let contract = Contract::from_json(
-        web3.eth(),
-        uni_contract_address,
-        include_bytes!("./build/IERC20.abi"),
-    )?;
-    // let balance: U256 = contract.query("balanceOf", (me,), None, Options::default(), None).await?;
-    // log::info!("{:?}", balance);
-
-    let filter = FilterBuilder::default()
-        .address(vec![contract.address()])
-        .topics(
-            Some(vec!["ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
-                .parse()
-                .unwrap()]),
-            None,
-            None,
-            None,
-        )
-        .build();
-
-    let filter = web3.eth_filter().create_logs_filter(filter).await?;
-
-    let logs_stream = filter.stream(time::Duration::from_secs(1));
-    futures::pin_mut!(logs_stream);
-
-    let log = logs_stream.next().await.unwrap();
-    println!("got log: {:?}", log);
-
+    environment::load();
+    uniswap::poll().await?;
     Ok(())
 }
---
+++ src/uniswap.rs
@@ -0,0 +1,60 @@
+extern crate hex;
+
+use super::environment;
+
+use web3::contract::Contract;
+use web3::futures::{future, StreamExt};
+use web3::types::{Address, FilterBuilder, Bytes};
+
+pub async fn poll() -> web3::contract::Result<()> {
+    let url = environment::get_value("ALCHEMY");
+    let transport = web3::transports::WebSocket::new(&url).await?;
+    let web3 = web3::Web3::new(transport);
+
+    let _uni_address: Address = "1f9840a85d5aF5bf1D1762F925BDADdC4201F984".parse().unwrap();
+    let _uni_router_address: Address = "7a250d5630B4cF539739dF2C5dAcb4c659F2488D".parse().unwrap();
+    let uni_eth_pair_address: Address = "d3d2E2692501A5c9Ca623199D38826e513033a17".parse().unwrap();
+    let uni_eth_pair_instance = Contract::from_json(
+        web3.eth(),
+        uni_eth_pair_address,
+        include_bytes!("./build/IUniswapV2Pair.abi"),
+    )?;
+
+    let filter = FilterBuilder::default()
+        .address(vec![uni_eth_pair_instance.address()])
+        .topics(
+            Some(vec!["d78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822"
+                .parse()
+                .unwrap()]),
+            None,
+            None,
+            None,
+        )
+        .build();
+
+    let sub = web3.eth_subscribe().subscribe_logs(filter).await?;
+
+    sub.for_each(|log| {
+        let log = log.unwrap();
+        log::info!("Transaction hash: {:?}", log.transaction_hash.unwrap());
+        let from = log.topics[1];
+        let to = log.topics[2];
+        
+        log::info!("{:?} -> {:?} ", from, to);
+        let Bytes(data) = log.data;
+        log::info!("{:?}", data);
+        future::ready(())
+    })
+    .await;
+
+    // let filter = web3.eth_filter().create_logs_filter(filter).await?;
+
+    // let logs_stream = filter.stream(time::Duration::from_secs(5));
+    // futures::pin_mut!(logs_stream);
+    
+    // loop {
+    //     let log = logs_stream.next().await.unwrap();
+    //     log::info!("got log: {:?}", log.unwrap());
+    // }
+    Ok(())
+}
