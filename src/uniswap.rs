fc03d756deab5336f2acaec421b9d508bb138ff4
Simon <simon.tan.yu.jing@gmail.com>
2020-11-20 14:17:53 +0800

    committing changes before continuing lol

--- src/uniswap.rs
+++ src/uniswap.rs
@@ -4,26 +4,55 @@ use super::environment;
 
 use web3::contract::Contract;
 use web3::futures::{future, StreamExt};
-use web3::types::{Address, FilterBuilder, Bytes, U256};
+use web3::types::{Address, FilterBuilder, Bytes, U256, Log};
+
+use std::collections::VecDeque;
+
+#[derive(Debug)]
+struct MovingSum {
+    interval: u64,
+    logs: VecDeque<Log>,  // Not to be confused with `log`
+    sum: U256,
+}
 
 pub async fn poll() -> web3::contract::Result<()> {
     let url = environment::get_value("ALCHEMY");
     let transport = web3::transports::WebSocket::new(&url).await?;
     let web3 = web3::Web3::new(transport);
 
-    let _uni_address: Address = "1f9840a85d5aF5bf1D1762F925BDADdC4201F984".parse().unwrap();
+    // TODO: Refactor constants into separate file
+    let token_name = "UNI";
+    let token_decimals = 18;
+    let token_address: Address = "1f9840a85d5aF5bf1D1762F925BDADdC4201F984".parse().unwrap();
     let _uni_router_address: Address = "7a250d5630B4cF539739dF2C5dAcb4c659F2488D".parse().unwrap();
-    let uni_eth_pair_address: Address = "d3d2E2692501A5c9Ca623199D38826e513033a17".parse().unwrap();
-    let uni_eth_pair_instance = Contract::from_json(
+    let token_eth_pair_address: Address = "d3d2E2692501A5c9Ca623199D38826e513033a17".parse().unwrap();
+    let event_signature_hash = "d78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822";
+    let token_eth_pair_instance = Contract::from_json(
         web3.eth(),
-        uni_eth_pair_address,
+        token_eth_pair_address,
         include_bytes!("./build/IUniswapV2Pair.abi"),
     )?;
+    
+    // I am tracking 10 blocks (~ 2 mins), 100 blocks (~ 20 mins) & 1000 blocks (~ 36 hours) moving sums (ms)
+    // If you want to compare the ms of 10 blocks w/ 1000 blocks, you need to divide
+    // The sum tracked for 1000 blocks by 100 to get average 10 block sums in the past 1000 blocks
+    // TODO: Potentially vert into Vector of moving sums for cleaner code.
+    let mut ten_blocks_buy_ms = MovingSum {
+        interval: 10,
+        logs: VecDeque::new(),
+        sum: U256::from(0),
+    };
+    let mut ten_blocks_sell_ms = MovingSum {
+        interval: 10,
+        logs: VecDeque::new(),
+        sum: U256::from(0),
+    };
+
 
     let filter = FilterBuilder::default()
-        .address(vec![uni_eth_pair_instance.address()])
+        .address(vec![token_eth_pair_instance.address()])
         .topics(
-            Some(vec!["d78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822"
+            Some(vec![event_signature_hash
                 .parse()
                 .unwrap()]),
             None,
@@ -31,42 +60,37 @@ pub async fn poll() -> web3::contract::Result<()> {
             None,
         )
         .build();
+    log::info!("Commencing logger");
+    log::info!("-------------------");
+    log::info!("Token name    : {}", token_name);
+    log::info!("Token address : 0x{}", token_address);
+    log::info!("-------------------");
+
 
     let sub = web3.eth_subscribe().subscribe_logs(filter).await?;
-
     sub.for_each(|log| {
         let log = log.unwrap();
-        log::info!("Transaction hash: {:?}", log.transaction_hash.unwrap());
-        // let H256(from) = log.topics[1];
-        // let H256(to) = log.topics[2];
-        // log::info!("0x{} -> 0x{} ", hex::encode(&from[12..]), hex::encode(&to[12..]));
+        // TODO: 1 VecDeque for log for buys
+        // TODO: 1 VecDequeu for log for sells
+        // TODO: 1 U256 for sum buys
+        // TODO: 1 U256 for sum sells
 
 
+        
+        log::info!("Tx hash : {:?}", log.transaction_hash.unwrap());
+        
         let Bytes(data) = log.data;
-        let uni_in: U256 = data[..32].into();
-        let eth_in: U256 = data[32..64].into();
-        let uni_out: U256 = data[64..96].into();
-        let eth_out: U256 = data[96..].into();
+        let uni_sold: U256 = data[..32].into();
+        let uni_bought: U256 = data[64..96].into();
 
-        //  / U256::from(10).pow(U256::from(18))
-        if uni_in > U256::from(0) {
-            log::info!("{:?} UNI -> {:?} ETH", uni_in, eth_out);
-        } else {
-            log::info!("{:?} ETH -> {:?} UNI", eth_in, uni_out);
-        }
+        let action: String = if uni_sold > U256::from(0) { "Sold    ".into() } else { "Bought  ".into() };
+        let qty = if uni_sold > U256::from(0) { uni_sold } else { uni_bought };
+        // Shadowing variable to make code more readable.
+        let qty = qty / U256::from(10).pow(U256::from(token_decimals));
+        log::info!("{}: {:?} UNI", action, qty);
 
         future::ready(())
     })
     .await;
-
-    // let filter = web3.eth_filter().create_logs_filter(filter).await?;
-
-    // let logs_stream = filter.stream(time::Duration::from_secs(5));
-    // futures::pin_mut!(logs_stream);
-    
-    // loop {
-    //     let log = logs_stream.next().await.unwrap();
-    //     log::info!("got log: {:?}", log.unwrap());
-    // }
     Ok(())
 }
