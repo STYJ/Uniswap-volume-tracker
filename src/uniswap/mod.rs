418a4d18f25124ea0b1dd0919d4ba2b1879dfa82
STYJ <simon.tan.yu.jing@gmail.com>
2020-11-24 14:19:27 +0800

    removed movingsum struct because I dont need interval field. Also added logic for getting unique combination of intervals (index) and WIP logic for getting sum of each interval

--- src/uniswap/mod.rs
+++ src/uniswap/mod.rs
@@ -20,7 +20,7 @@ pub async fn poll() -> web3::contract::Result<()> {
     let web3 = web3::Web3::new(transport);
     let token_eth_pair_address: Address = TOKEN_ETH_PAIR_ADDRESS.parse().unwrap();
 
-    let token_eth_pair_instance = Contract::from_json(
+    let _token_eth_pair_instance = Contract::from_json(
         web3.eth(),
         token_eth_pair_address,
         include_bytes!("../build/IUniswapV2Pair.abi"),
@@ -32,35 +32,22 @@ pub async fn poll() -> web3::contract::Result<()> {
     println!("Token address : 0x{}", TOKEN_ADDRESS);
     println!("-------------------");
 
-    // let (mut buy_moving_sum, mut sell_moving_sum) = init_moving_sums();
-    // add_past_transactions_to_moving_sums(&mut buy_moving_sum, &mut sell_moving_sum, &web3).await?;
-    // subscribe(&mut buy_moving_sum, &mut sell_moving_sum, &web3).await?;
+    let mut buy_logs: VecDeque<MinimalTx> = VecDeque::new();
+    let mut sell_logs: VecDeque<MinimalTx> = VecDeque::new();
+
+    add_past_transactions(&mut buy_logs, &mut sell_logs, &web3).await?;
+    subscribe(&mut buy_logs, &mut sell_logs, &web3).await?;
     get_unique_interval_combinations();
     Ok(())
 }
 
-// Initialises an empty vector of MovingSums
-// Intervals are 10, 100 and 1000
-fn init_moving_sums() -> (MovingSum, MovingSum) {
-    println!("1. Initialise moving sums");
-    let buy_moving_sum: MovingSum = MovingSum {
-        interval: *INTERVALS.last().unwrap(),
-        logs: VecDeque::new(),
-    };
-    let sell_moving_sum: MovingSum = MovingSum {
-        interval: *INTERVALS.last().unwrap(),
-        logs: VecDeque::new(),
-    };
-    (buy_moving_sum, sell_moving_sum)
-}
-
-// Fill moving sums with historical transactions
-async fn add_past_transactions_to_moving_sums(
-    buy_moving_sum: &mut MovingSum,
-    sell_moving_sum: &mut MovingSum,
+// 
+async fn add_past_transactions(
+    buy_logs: &mut VecDeque<MinimalTx>,
+    sell_logs: &mut VecDeque<MinimalTx>,
     web3: &web3::Web3<web3::transports::WebSocket>,
 ) -> web3::contract::Result<()> {
-    println!("2. Querying past events...");
+    println!("1. Querying past events...");
     let token_eth_pair_address: Address = TOKEN_ETH_PAIR_ADDRESS.parse().unwrap();
     let mut curr_block = web3.eth().block_number().await?;
     let mut from_block = curr_block - U64::from(*INTERVALS.last().unwrap());
@@ -86,7 +73,7 @@ async fn add_past_transactions_to_moving_sums(
             .build();
         let events = web3.eth().logs(filter).await?;
         for log in events {
-            push_log_to_ms(buy_moving_sum, sell_moving_sum, log);
+            push_log(buy_logs, sell_logs, log);
         }
         from_block += U64::from(NUM_BLOCKS_PER_QUERY);
         curr_block = web3.eth().block_number().await?;
@@ -95,11 +82,11 @@ async fn add_past_transactions_to_moving_sums(
 }
 
 async fn subscribe(
-    buy_moving_sum: &mut MovingSum,
-    sell_moving_sum: &mut MovingSum,
+    buy_logs: &mut VecDeque<MinimalTx>,
+    sell_logs: &mut VecDeque<MinimalTx>,
     web3: &web3::Web3<web3::transports::WebSocket>,
 ) -> web3::contract::Result<()> {
-    println!("3. Listening for new events...");
+    println!("2. Listening for new events...");
     let token_eth_pair_address: Address = TOKEN_ETH_PAIR_ADDRESS.parse().unwrap();
     let filter = FilterBuilder::default()
         .address(vec![token_eth_pair_address])
@@ -113,14 +100,14 @@ async fn subscribe(
     let sub = web3.eth_subscribe().subscribe_logs(filter).await?;
     sub.for_each(|log| {
         let log = log.unwrap();
-        push_log_to_ms(buy_moving_sum, sell_moving_sum, log);
+        push_log(buy_logs, sell_logs, log);
         future::ready(())
     })
     .await;
     Ok(())
 }
 
-fn push_log_to_ms(buy_moving_sum: &mut MovingSum, sell_moving_sum: &mut MovingSum, log: Log) {
+fn push_log(buy_logs: &mut VecDeque<MinimalTx>, sell_logs: &mut VecDeque<MinimalTx>, log: Log) {
     let Bytes(data) = log.data;
     let uni_sold: U256 = data[..32].into();
     let uni_bought: U256 = data[64..96].into();
@@ -132,37 +119,57 @@ fn push_log_to_ms(buy_moving_sum: &mut MovingSum, sell_moving_sum: &mut MovingSu
 
     if uni_sold > U256::from(0) {
         latest_tx.qty = uni_sold;
-        add(sell_moving_sum, latest_tx);
+        add_and_pop(sell_logs, latest_tx);
     } else {
         latest_tx.qty = uni_bought;
-        add(buy_moving_sum, latest_tx);
+        add_and_pop(buy_logs, latest_tx);
     }
 }
 
-fn add(moving_sum: &mut MovingSum, minimal_tx: MinimalTx) {
-    moving_sum.logs.push_back(minimal_tx);
-    // Remove transactions that fall outside of latest block number - interval
+fn add_and_pop(logs: &mut VecDeque<MinimalTx>, minimal_tx: MinimalTx) {
+    logs.push_back(minimal_tx);
+    // Remove transactions that fall outside of latest block number - largest interval
     loop {
-        let first_block = moving_sum.logs.front().unwrap().block;
-        let last_block = moving_sum.logs.back().unwrap().block;
+        let first_block = logs.front().unwrap().block;
+        let last_block = logs.back().unwrap().block;
         let block_diff = last_block - first_block;
-        if block_diff > U64::from(moving_sum.interval) {
-            moving_sum.logs.pop_front();
+        if block_diff > U64::from(*INTERVALS.last().unwrap()) {
+            logs.pop_front();
         } else {
             break;
         }
     }
 }
 
+// fn get_sum_for_each_interval(logs: &VecDeque<MinimalTx>) -> [U256; 3] {
+//     let mut sums: [U256; 3] = [U256::from(0); 3];
+//     // i is for interating logs
+//     // j is for iterating intervals
+//     let mut i = logs.len() - 2;
+//     let mut j = 0;
+//     let last = logs.back().unwrap();
+//     let last_block = last.block;
+//     let mut sum = last.qty;
+//     while i >= 0 && j < INTERVALS.len() {
+//         let curr_block = logs[i].block;
+//         if last_block - curr_block < U64::from(*INTERVALS.last().unwrap()) {
+            
+//         }
+
+
+//         i -= 1;
+//     }
+//     sums
+// }
+
 fn get_unique_interval_combinations() -> VecDeque<(u64, u64)> {
-    let num_intervals = INTERVALS.len();
-    let mut i = 0;
-    let mut j = 1;
+    let mut i: u64 = 0;
+    let mut j: u64 = 1;
     let mut combinations: VecDeque<(u64, u64)> = VecDeque::new();
-    while i < num_intervals && j < num_intervals && i < j {
-        combinations.push_back((INTERVALS[i], INTERVALS[j]));
+    while i < NUM_INTERVALS && j < NUM_INTERVALS && i < j {
+        combinations.push_back((i, j));
         j += 1;
-        if(j == num_intervals) {
+        if j == NUM_INTERVALS {
             i += 1;
             j = i + 1;
         }
