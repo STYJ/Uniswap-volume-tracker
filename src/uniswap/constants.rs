02a4d8801cbb70c72fa50c213eab0eb61bd15545
STYJ <simon.tan.yu.jing@gmail.com>
2020-11-26 09:58:51 +0800

    testing other constants

--- src/uniswap/constants.rs
+++ src/uniswap/constants.rs
@@ -1,10 +1,10 @@
 use web3::types::{H256, U256, U64};
 
-pub const TOKEN_NAME: &str = "UNI";
+pub const TOKEN_NAME: &str = "STAKE";
 pub const TOKEN_DECIMALS: i8 = 18;
-pub const TOKEN_ADDRESS: &str = "1f9840a85d5aF5bf1D1762F925BDADdC4201F984";
+pub const TOKEN_ADDRESS: &str = "0ae055097c6d159879521c384f1d2123d1f195e6";
 pub const _UNI_ROUTER_ADDRESS: &str = "7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
-pub const TOKEN_ETH_PAIR_ADDRESS: &str = "d3d2E2692501A5c9Ca623199D38826e513033a17";
+pub const TOKEN_ETH_PAIR_ADDRESS: &str = "3b3d4eefdc603b232907a7f3d0ed1eea5c62b5f7";
 pub const EVENT_SIGNATURE_HASH: &str = "d78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822";
 
 pub const NUM_INTERVALS: usize = 4;
